7763c4af1f6833f59d7efe60cec5c4ba382d9c9a
Bohdan Yablinchuk 44987 <yablinchuk9@gmail.com>
2024-03-25 20:23:43 +0100

    commit

--- lab3/zad1/app.js
+++ lab3/zad1/app.js
@@ -1,8 +1,12 @@
+
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.send(`
