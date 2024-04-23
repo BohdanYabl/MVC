1275e4c232546a183682dd3b28afbf204ea6aaa7
Bohdan Yablinchuk 44987 <yablinchuk9@gmail.com>
2024-04-23 09:16:40 +0200

    modified

--- lab7/zad1/app.js
+++ lab7/zad1/app.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(session({
   secret: 'secret',
--- lab7/zad2/app.js
+++ lab7/zad2/app.js
@@ -4,36 +4,29 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 4001;
+const PORT = 3000;
 
-// Підключення маршрутів
 const userRoutes = require('./routes/user');
 const bookRoutes = require('./routes/book');
 const errorRoutes = require('./routes/error');
 
-// Встановлення каталогу статичних файлів
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Встановлення двигуна для шаблонів
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Налаштування сесій
 app.use(session({
   secret: 'secret',
   resave: false,
   saveUninitialized: true
 }));
 
-// Використання bodyParser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Підключення маршрутів до програми
 app.use('/user', userRoutes);
 app.use('/books', bookRoutes);
 app.use('*', errorRoutes);
 
-// Прослуховування порту
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
